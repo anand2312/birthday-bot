4c8e0fcf6cd6a70c9d158a6a359f564af68c138f
anand2312 <anandkrishna2312@gmail.com>
2021-12-25 16:56:53 +0400

    deps: add jishaku

--- bdaybot/__main__.py
+++ bdaybot/__main__.py
@@ -2,11 +2,23 @@ import os
 import pathlib
 
 from dotenv import load_dotenv
+from loguru import logger
 
 from bdaybot import bot
 
 
 load_dotenv(pathlib.Path(__file__).parent.parent / ".env")
+
+
+EXTENSIONS = {"jishaku"}  # add more here as you go
+
+for ext in EXTENSIONS:
+    try:
+        bot.load_extension(ext)
+    except Exception as e:
+        logger.exception(f"Could not load extension: {ext} | {e}")
+    else:
+        logger.info(f"Loaded {ext} extension succesfully")
 
 
 bot.run(os.environ.get("TOKEN"))
